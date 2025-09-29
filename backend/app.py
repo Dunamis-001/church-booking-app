b4de0d914e85abd402df34697d8f00014c75b76c
Dunamis-001 <alex.nyamai@student.moringaschool.com>
2025-09-29 16:56:17 +0300

    deployment config 2

--- backend/app.py
+++ backend/app.py
@@ -13,33 +13,33 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def create_app():
-    app = Flask(__name__, static_url_path="/", static_folder="./client/dist")
-    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-    app.config.from_object(Config)
-    
-    # Initialize extensions
-    db.init_app(app)
-    migrate = Migrate(app, db)
-    bcrypt = Bcrypt(app)
-    jwt = JWTManager(app)
-    CORS(app, resources={r"/*": {"origins": Config.CORS_ORIGINS}})
-    
-    # Register blueprints
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(rooms_bp)
-    app.register_blueprint(bookings_bp)
-    
-    @app.route("/")
-    def index():
-        return app.send_static_file("index.html")
 
-    @app.errorhandler(404)
-    def not_found(err):
-        return app.send_static_file("index.html")
-    
-    return app
+app = Flask(__name__, static_url_path="/", static_folder="./client/dist")
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config.from_object(Config)
+
+# Initialize extensions
+db.init_app(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+CORS(app, resources={r"/*": {"origins": Config.CORS_ORIGINS}})
+
+# Register blueprints
+app.register_blueprint(auth_bp)
+app.register_blueprint(rooms_bp)
+app.register_blueprint(bookings_bp)
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
+
+@app.errorhandler(404)
+def not_found(err):
+    return app.send_static_file("index.html")
+
+
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, port=5000)
+
+    app.run(debug=False, port=5000)
