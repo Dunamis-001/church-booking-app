a0e56da1406c4d714ea7c79151e52c9779d88c17
Dunamis-001 <alex.nyamai@student.moringaschool.com>
2025-09-29 16:41:45 +0300

    Delete client directory

--- client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="bg-gray-800 text-white py-6 text-center">
-    <p>&copy; 2024 Church Booking. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
